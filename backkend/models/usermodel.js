e5d9eadc55059b9799da7a35008c3a407390e2e3
work.amansingh12@gmail.com <work.amansingh12@gmail.com>
2026-05-01 03:47:33 +0530

    fix userSchema not defined error

--- backkend/models/usermodel.js
+++ backkend/models/usermodel.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 
-const userModel = new mongoose.Schema({
-    fullName:{
-        type:String,
-        required:true
-    },
-    username:{
-      type:String,
-      required:true,
-      unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    profilePhoto:{
-        type:String,
-        default:""
-    },
-    gender:{
-        type:String,
-        enum:["male", "female"],
-        required:true
-    },
-    lastSeen: {
-        type: Date,
-        default: Date.now
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date
-    
-}, {timestamps:true});
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePhoto: {
+    type: String,
+    default: ""
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true
+  },
+
+  // last seen feature
+  lastSeen: {
+    type: Date,
+    default: Date.now
+  }
+
+}, { timestamps: true });
+
 export default mongoose.model("User", userSchema);
