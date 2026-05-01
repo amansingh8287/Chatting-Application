5abdf9f8e6992ce54625df6e8eedac1390f156d7
work.amansingh12@gmail.com <work.amansingh12@gmail.com>
2026-05-01 22:30:18 +0530

    reducer fix

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,35 +1,47 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name:"user",
-    initialState:{
-        authUser:null,
-        otherUsers:[],
-        selectedUser:null,
-        onlineUsers:[],
-        profileUser: null,
-        showProfile: false
+  name: "user",
+  initialState: {
+    authUser: null,
+    otherUsers: [],
+    selectedUser: null,
+    onlineUsers: [],
+    users: [],
+    profileUser: null,
+    showProfile: false,
+  },
+  reducers: {
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
     },
-    reducers:{
-        setAuthUser:(state,action)=>{
-            state.authUser = action.payload;
-        },
-        setOtherUsers:(state, action)=>{
-            state.otherUsers = action.payload;
-        },
-        setSelectedUser:(state,action)=>{
-            state.selectedUser = action.payload;
-        },
-        setOnlineUsers:(state,action)=>{
-            state.onlineUsers = action.payload;
-        },
-        setProfileUser: (state, action) => {
-           state.profileUser = action.payload;
-        },
-        setShowProfile: (state, action) => {
-          state.showProfile = action.payload;
-        }
-    }
+    setOtherUsers: (state, action) => {
+      state.otherUsers = action.payload;
+    },
+    setSelectedUser: (state, action) => {
+      state.selectedUser = action.payload;
+    },
+    setUsers: (state, action) => {
+      state.users = action.payload;
+    },
+    setOnlineUsers: (state, action) => {
+      state.onlineUsers = action.payload;
+    },
+    setProfileUser: (state, action) => {
+      state.profileUser = action.payload;
+    },
+    setShowProfile: (state, action) => {
+      state.showProfile = action.payload;
+    },
+  },
 });
-export const {setAuthUser,setOtherUsers,setSelectedUser,setOnlineUsers, setProfileUser,setShowProfile} = userSlice.actions;
+export const {
+  setAuthUser,
+  setOtherUsers,
+  setUsers,
+  setSelectedUser,
+  setOnlineUsers,
+  setProfileUser,
+  setShowProfile,
+} = userSlice.actions;
 export default userSlice.reducer;
